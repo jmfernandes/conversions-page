a7ca0780869b4d01fe944ef64d499bb4ab657761
Josh Fernandes <joshfernandes@mac.com>
2012-10-25 23:53:18 -0700

    changed name

--- app3.py
+++ app3.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    return  render_template('conversions.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
