8398b819c46abc5190622c30734126d4f51bb804
Josh Fernandes <joshfernandes@mac.com>
2012-11-06 20:16:44 -0800

    added link

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app3.py
+web: python app.py
---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import json
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+@app.route('/')
+def index():
+    return  render_template('conversions.html')
+
+@app.route('/physics/ev_to_joules', endpoint='ev_to_joules')
+def index():
+    return  render_template('ev_to_joules.html')
+
+@app.route('/physics/ev_to_joules_json', endpoint='ev_to_joules_json')
+def index():
+    return  render_template('joule/ev_to_joules.json')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
