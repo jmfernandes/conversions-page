6976c66a994b74d74ba4575ddbf73980734f9a2b
Josh Fernandes <joshfernandes@mac.com>
2013-06-15 16:23:02 -0700

    figuring out get and post

--- templates/conversions.php
+++ templates/conversions.php
@@ -1,2 +1,3 @@
-you entered <?php echo $_GET["numbers"]; ?>.<br>
-the units are <?php echo $_GET["units"]; ?>.
+you entered <?php echo $_GET["number"]; ?>.<br>
+the units are <?php echo $_GET["units"]; ?>.<br>
+the final units are <?php echo $_GET["result"]; ?>
