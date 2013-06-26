efab0a7e7452f477842fe960e946d47815fa289c
Josh Fernandes <joshfernandes@mac.com>
2013-06-25 21:35:16 -0700

    working with post

--- app.py
+++ app.py
@@ -32,8 +32,12 @@ def index():
 
 @app.route('/data', methods =['GET','POST'])
 def handle_data():
-    #data = {'num':0, 'unit':'', 'res':0}
+    data = {'num':0, 'unit':'', 'res':0}
     data['num'] = request.form.get('units',None)
+    json_file = open('templates/json/ev_to_joules.json')
+    jsondata = jsonload(json_file)
+    json_file.close()
+    """modify data['num'] with json data""""
     #if request.method == 'POST':
     #if reqeust.form.get('units',None) == meters:
         #data = 6
