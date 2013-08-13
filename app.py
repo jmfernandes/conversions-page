f18d64e4eddfa9c5e842b7d4be8158ed738f0362
Josh Fernandes <joshfernandes@mac.com>
2013-08-13 14:11:14 -0700

    smoothing edges

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ def page_not_found(error):
 def index():
     global foo
     foo = request.form.get('units',None)
-    data = {'num':0, 'unit':'', 'unitres':foo, 'res':'', 'mainunit':''}
+    boo = request.form.get('resultunit',None)
+    data = {'num':0, 'unit':foo, 'unitres':boo, 'res':'', 'mainunit':''}
     data['unit'] = request.form.get('units',None)
     data['unitres'] = request.form.get('resultunit',None)
     data['mainunit'] = request.form.get('maincat',None)
@@ -42,7 +43,7 @@ def index():
 
 @app.route('/data', methods =['GET','POST'])
 def handle_data():
-    data = {'num':0, 'unit':'', 'unitres':foo, 'res':'', 'mainunit':''}
+    data = {'num':0, 'unit':foo, 'unitres':boo, 'res':'', 'mainunit':''}
     data['num'] = request.form.get('number',None)
     data['unit'] = request.form.get('units',None)
     data['unitres'] = request.form.get('resultunit',None)
